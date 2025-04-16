be2175b04e7b16a5af86057c9907069851555867
apiyOsusan <apiyos804@gmail.com>
2025-04-16 04:38:43 -0700

    Commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function knowMore() {
+    alert('Clicked on Know More!');
+    // You can add more functionality here, such as scrolling to a specific section or opening a modal.
+}
+
+function downloadCV() {
+    alert('Clicked on Download CV!');
+    // You can add more functionality here, such as triggering a file download.
+}
